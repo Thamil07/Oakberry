c57124b664bbfe06c1ea5b434ab5a2368c9bcc0a
Thamil07 <123970833+Thamil07@users.noreply.github.com>
2024-11-14 11:14:23 +0530

    Delete server directory

--- server/Routers/Refresh_Router.js
+++
@@ -1,7 +0,0 @@
-const express=require("express")
-const {createaccesstoken}=require("../Controllers/Authentication")
-const router=express.Router();
-
-router.post("/refresh",createaccesstoken)
-
-module.exports=router;
